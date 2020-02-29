54f47090a869313369addde2aff5ded70945c5d6
Chang Liu <chang.liu@redfin.com>
2020-02-29 15:30:44 -0800

    [feat] Add photo FS support with based64 data

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -5,31 +5,74 @@ import { useStorage } from '@ionic/react-hooks/storage';
 import { isPlatform } from '@ionic/react';
 import { CameraResultType, CameraSource, CameraPhoto, Capacitor, FilesystemDirectory } from "@capacitor/core";
 
+const PHOTO_STORAGE = "photos";
+
 export function usePhotoGallery() {
+  const { get, set } = useStorage();
+  const { getPhoto } = useCamera();
+  const { deleteFile, getUri, readFile, writeFile } = useFilesystem();
 
-    const { getPhoto } = useCamera();
-    const [photos, setPhotos] = useState<Photo[]>([]);
-  
-    const takePhoto = async () => {
-        const cameraPhoto = await getPhoto({
-        resultType: CameraResultType.Uri,
-        source: CameraSource.Camera,
-        quality: 100
+  const [photos, setPhotos] = useState<Photo[]>([]);
+
+  useEffect(() => {
+    const loadSaved = async () => {
+      const photosString = await get('photos');
+      const photos = (photosString ? JSON.parse(photosString) : []) as Photo[];
+      for (let photo of photos) {
+        const file = await readFile({
+          path: photo.filepath,
+          directory: FilesystemDirectory.Data
         });
-
-        const fileName = new Date().getTime() + '.jpeg';
-        const newPhotos = [{
-            filepath: fileName,
-            webviewPath: cameraPhoto.webPath
-        }, ...photos];
-
-        setPhotos(newPhotos)
+        photo.base64 = `data:image/jpeg;base64,${file.data}`;
+      }
+      setPhotos(photos);
     };
-  
+
+    loadSaved();
+  }, [get, readFile]);
+
+  const takePhoto = async () => {
+    const cameraPhoto = await getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFileImage = await savePicture(cameraPhoto, fileName);
+    const newPhotos = [savedFileImage, ...photos];
+
+    setPhotos(newPhotos);
+    set(PHOTO_STORAGE, JSON.stringify(newPhotos.map(p => {
+      // Don't save the base64 representation of the photo data, 
+      // since it's already saved on the Filesystem
+      const photoCopy = { ...p };
+      delete photoCopy.base64;
+      return photoCopy;
+    })));
+  };
+
+  const savePicture = async (photo: CameraPhoto, fileName: string) => {
+    const base64Data = await base64FromPath(photo.webPath!);
+    await writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+    });
+    return getPhotoFile(photo, fileName);
+  };
+
+  const getPhotoFile = async (cameraPhoto: CameraPhoto, fileName: string): Promise<Photo> => {
     return {
-        photos,
-        takePhoto
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath
     };
+  };
+
+  return {
+    photos,
+    takePhoto
+  };
 }
 
 export interface Photo {
